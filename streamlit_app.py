fad69da9007da753992603e30e1ecfabe65df583
vjvhedx <129539675+vjvhedx@users.noreply.github.com>
2023-03-31 13:11:04 -0400

    Update streamlit_app.py
    Added menu details

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,11 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+
+streamlit.header('Breakfast Menu')
+
+streamlit.title('Omega 3 and Blueberry Oatmeal')
+
+streamlit.title('Kale, Spinach and Rocket Smoothie')
+
+streamlit.title('Hard boiled free range egg')
