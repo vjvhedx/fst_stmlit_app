a2775e4cfe0971e782ef6923ea7f63dbe3110750
vjvhedx <129539675+vjvhedx@users.noreply.github.com>
2023-03-31 13:13:37 -0400

    Update streamlit_app.py
    Added emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,8 +4,10 @@ streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
 
-streamlit.text('Omega 3 and Blueberry Oatmeal')
+streamlit.text('🥣 Omega 3 and Blueberry Oatmeal')
 
-streamlit.text('Kale, Spinach and Rocket Smoothie')
+streamlit.text('🥗 Kale, Spinach and Rocket Smoothie')
 
-streamlit.text('Hard boiled free range egg')
+streamlit.text('🐔 Hard boiled free range egg')
+
+streamlit.text('🥑🍞 Avocado Toast')
